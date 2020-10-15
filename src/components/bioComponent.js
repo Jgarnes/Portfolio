e59d45bc39403377543b244ab05bfdc9dbbd79e7
Jeremy Garnes <J06Garnes@gmail.com>
2020-10-15 02:02:27 -0400

    Added About Me Section

--- src/components/bioComponent.js
+++ src/components/bioComponent.js
@@ -1,23 +1,23 @@
 import React from 'react';
+import { Container, Row, Col} from 'reactstrap';
 
 function Bio() {
     return (
-        <section id="bio" class="main">
-        <div class="spotlight">
-            <div class="content">
-                <header class="major">
-                    <h2>Ipsum sed adipiscing</h2>
-                </header>
-                <p>Sed lorem ipsum dolor sit amet nullam consequat feugiat consequat magna
-                adipiscing magna etiam amet veroeros. Lorem ipsum dolor tempus sit cursus.
-                Tempus nisl et nullam lorem ipsum dolor sit amet aliquam.</p>
-                <ul class="actions">
-                    <li><a href="generic.html" class="button">Learn More</a></li>
-                </ul>
-            </div>
-            <span class="image"><img src="images/pic01.jpg" alt="" /></span>
-        </div>
-    </section>
+        <Container id="bio">
+            <Row>
+                <Col>
+            <h1 className="heading alt">About Me</h1>
+                </Col>
+            </Row>
+            <Row className="row-content alt">
+                <Col>
+                    <p>Transitioning my skills from over 5 years’ experience in theater, event production, and production management to a career in
+                    website development. Proficient with various front-end and back-end frameworks as well as management of underlying
+                    databases. Professional and detail-oriented with strong leadership and communication skills. Proven ability to plan, organize,
+                    and execute, to meet both customer expectations and organizational goals.</p>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
