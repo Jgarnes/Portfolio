f08a6dd36eeca564e5dc6773719370a67a95a086
Jeremy Garnes <J06Garnes@gmail.com>
2020-11-25 17:44:40 -0500

    Added Animation

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Header() {
     return (
     <header id="header" className="alt">
-        <div className="logo"><img src="/photos/headshot.jpg"  alt="" /></div>
+        <div className="logo" data-aos="fade-down"  data-aos-duration="3000" data-aos-easing="ease-in-out"><img src="/photos/headshot.jpg"  alt="" /></div>
         <h1>Hello, My name is Jeremy Garnes <br/></h1> 
-        <p>I am a Web/Mobile developer</p>    
+        <p>I am a Full Stack Developer</p>    
     </header>
     );
 }
--- src/components/recentWorkComponent.js
+++ src/components/recentWorkComponent.js
@@ -16,13 +16,13 @@ function Projects() {
                 </Col>
             </Row>
             <Row className="row-content">
-                <Col md="6">
+                <Col md="6" data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease-in-out">
                     <Media className="mid" href="#">
                         <a href="https://romantic-allen-8674b4.netlify.app/" target="_blank" rel="noopener noreferrer"><Media height="325px" width="100%" className="photo" object src="/photos/thermo.jpg" alt="" /></a>
                     </Media>
                 </Col> 
-                <Col md="6" style={{marginTop:"auto", marginBottom:"auto"}}>       
-                    <Media body className="alt">
+                <Col md="6" data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease-in-out" style={{marginTop:"auto", marginBottom:"auto"}}>       
+                    <Media body className="alt" >
                         <Media heading>
                             Weather App
                         </Media>
@@ -32,12 +32,12 @@ function Projects() {
                 </Col>
             </Row>
             <Row className="row-content">
-                <Col md="6 order-md-12">    
+                <Col md="6 order-md-12" data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease-in-out"  >    
                     <Media className="mid">
                        <a href="https://xenodochial-golick-7b4fc1.netlify.app/" target="_blank" rel="noopener noreferrer"> <Media height="325px" width="100%" className="photo" object src="/photos/calc.jpg" alt=""/></a>
                     </Media>
                 </Col>
-                <Col md="6" style={{marginTop:"auto", marginBottom:"auto"}}>
+                <Col md="6" data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease-in-out"  style={{marginTop:"auto", marginBottom:"auto"}}>
                     <Media body className="alt">
                         <Media heading>
                             Great Scott!!! 
@@ -47,12 +47,12 @@ function Projects() {
                 </Col>
             </Row>    
             <Row className="row-content">
-                <Col md="6">
+                <Col md="6" data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease-in-out">
                     <Media className="mid" href="#">
                         <a href="https://jeremygarnes.netlify.app/" target="_blank" rel="noopener noreferrer"><Media height="325px" width="100%" className="photo" object src="/photos/folio.jpg" alt="" /></a>
                     </Media>
                 </Col>
-                <Col md="6" style={{marginTop:"auto", marginBottom:"auto"}}>
+                <Col md="6" data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease-in-out" style={{marginTop:"auto", marginBottom:"auto"}}>
                     <Media body className="alt">
                         <Media heading>
                             Portfolio V 0.5
